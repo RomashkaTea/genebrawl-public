9aaa27b7a1ca93a910fe046ec5648aef3d3c3c2c
Romashka <80623570+RomashkaTea@users.noreply.github.com>
2026-07-29 23:05:34 +0300

    Libc: fix missing exports

--- src/libs/Libc.ts
+++ src/libs/Libc.ts
@@ -1,15 +1,14 @@
-
 const PROP_VALUE_MAX = 256;
 
 export class Libc {
     static _open = new NativeFunction(Module.getGlobalExportByName("open")!, 'int', ['pointer', 'int', 'int']);
-    static _system_property_get = new NativeFunction(Module.getGlobalExportByName("__system_property_get")!, 'int', ['pointer', 'pointer']);
+    static _system_property_get: NativeFunction<number, [NativePointerValue, NativePointerValue]> | null = null;
     static _remove = new NativeFunction(Module.getGlobalExportByName('remove')!, 'int', ['pointer']);
     static _mkdir = new NativeFunction(Module.getGlobalExportByName('mkdir')!, 'int', ['pointer', 'int']);
     static _chmod = new NativeFunction(Module.getGlobalExportByName('chmod')!, 'int', ['pointer', 'int']);
     static _access = new NativeFunction(Module.getGlobalExportByName('access')!, 'int', ['pointer', 'int']);
     static _opendir = new NativeFunction(Module.getGlobalExportByName('opendir')!, 'pointer', ['pointer']);
-
+    static _sysctlbyname: NativeFunction<number, [NativePointerValue, NativePointerValue, NativePointerValue, NativePointerValue, number]> | null = null;
     static getaddrinfo = new NativeFunction(Module.getGlobalExportByName("getaddrinfo")!, 'int', ['pointer', 'pointer', 'pointer', 'pointer']);
     static close = new NativeFunction(Module.getGlobalExportByName("close")!, 'void', ['int']);
     static free = new NativeFunction(Module.getGlobalExportByName("free")!, 'void', ['pointer']);
@@ -26,9 +25,15 @@ export class Libc {
     static read = new NativeFunction(Module.getGlobalExportByName("read")!, 'int', ['int', 'pointer', 'int']);
 
     static getSystemProperty(prop: string): string {
+        if (!this._system_property_get) {
+            let exportPtr = Module.getGlobalExportByName("__system_property_get");
+            if (!exportPtr) return "";
+            this._system_property_get = new NativeFunction(exportPtr, 'int', ['pointer', 'pointer']);
+        }
+
         let value = this.malloc(PROP_VALUE_MAX);
 
-        this._system_property_get(prop.ptr(), value);
+        this._system_property_get!(prop.ptr(), value);
 
         let result = value.readUtf8String();
 
@@ -38,13 +43,19 @@ export class Libc {
     }
 
     static sysctlbyname(name: string): string {
+        if (!this._sysctlbyname) {
+            let exportPtr = Module.getGlobalExportByName("sysctlbyname");
+            if (!exportPtr) return "";
+            this._sysctlbyname = new NativeFunction(exportPtr, 'int', ['pointer', 'pointer', 'pointer', 'pointer', 'int']);
+        }
+
         let value = this.malloc(PROP_VALUE_MAX);
         let lengthPtr = this.malloc(4);
         lengthPtr.writeInt(PROP_VALUE_MAX);
 
         let resultStr = "";
 
-        let result = new NativeFunction(Module.getGlobalExportByName("sysctlbyname")!, 'int', ['pointer', 'pointer', 'pointer', 'pointer', 'int'])(name.ptr(), value, lengthPtr, NULL, 0);
+        let result = this._sysctlbyname(name.ptr(), value, lengthPtr, NULL, 0);
         if (result != -1) {
             resultStr = value.readUtf8String()!;
         }
