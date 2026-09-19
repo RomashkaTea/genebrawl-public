b9002cd08227c0a56c2f333ff96639b5df9ed15c
Romashka <80623570+RomashkaTea@users.noreply.github.com>
2026-09-20 00:32:33 +0300

    TeamManager: remove dead code

--- src/logic/home/team/TeamManager.ts
+++ src/logic/home/team/TeamManager.ts
@@ -22,12 +22,6 @@ const TeamManager_openTeamChat = new NativeFunction(
 // 4FD790
 const TeamManager_instance = Libg.offset(0x103D9A8, 0xEE6200); // 1st dword after "HomePageTeamMember::onSpeechBubblePressed() missing chat entry"
 
-const TeamManager_onTeamStream_JUMPOUTFROM = Libg.offset(0x4FF0D0, -1); // "Got team stream message %i"
-const TeamManager_onTeamStream_JUMPOUTTO = Libg.offset(0x4FF0EC, -1); // "Got team stream message %i"
-
-const TeamManager_patch1 = Libg.offset(0x798784, -1);  // if (v9 === 1) (asm: CMP W8, #1)
-const TeamManager_patch2 = Libg.offset(0x79BDD0, -1); // if (v5 == 5) (asm: CMP W22, #5) ("TeamChatPinned")
-
 
 export class TeamManager {
     static getInstance() {
@@ -53,33 +47,5 @@ export class TeamManager {
         Interceptor.replace(TeamManager_checkIsReadyAndPrint, new NativeCallback(function (teamManager) {
             return 0;
         }, 'void', ['pointer']));
-
-        if (!LogicDefines.isPlatformIOS()) {
-            Armceptor.jumpout(TeamManager_onTeamStream_JUMPOUTFROM, TeamManager_onTeamStream_JUMPOUTTO);
-
-            Interceptor.attach(TeamManager_patch1, function () {
-                const check = (this.context as Arm64CpuContext);
-
-                if (check.x8.equals(1) && !GameStateManager.isHomeMode()) {
-                    (this.context as Arm64CpuContext).x8 = NULL;
-                }
-            });
-            if (LogicDefines.isPlatformIOS()) {
-                Interceptor.attach(TeamManager_patch2, function () {
-                    const check = (this.context as Arm64CpuContext);
-
-                    if (check.x22.equals(5) && !GameStateManager.isHomeMode()) {
-                        (this.context as Arm64CpuContext).x22 = NULL;
-                    }
-                });
-            }
-        }
-        else {
-            console.warn("Memory.patchCode will not work on iOS!!! Find another way to patch this. да мне похуй бля");
-        }
-    }
-
-    static openTeamChat() {
-        TeamManager_openTeamChat(); // maybe rewrite function, so we can close chat on battleend?
     }
 }
